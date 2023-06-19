2d05ba53ba56384231945d503d4d789015a9f748
yoavharlap <yoavharlap1@gmail.com>
2023-06-19 21:34:14 +0300

    19.06

--- NN_torch_functions.py
+++ NN_torch_functions.py
@@ -1,5 +1,6 @@
 import torch
 
+
 def euclidean_distance(vectors):
     # unpack the vectors into separate tensors
     featsA, featsB = vectors
@@ -8,3 +9,35 @@ def euclidean_distance(vectors):
     # return the euclidean distance between the tensors
     # return torch.sqrt(torch.max(sumSquared, torch.tensor(torch.finfo(sumSquared.dtype).eps)))
     return sumSquared
+
+
+import torch
+import torch.nn as nn
+
+
+class ContrastiveLoss(nn.Module):
+    def __init__(self, margin=1):
+        super(ContrastiveLoss, self).__init__()
+        self.margin = margin
+
+
+        # label = 0 --> not similiar
+
+    def forward(self, output1, output2, label):
+        euclidean_distance = torch.nn.functional.pairwise_distance(output1, output2)
+        loss_contrastive = torch.mean((label) * torch.pow(euclidean_distance, 2) +
+                                      (1 - label) * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0),
+                                                              2))
+        return loss_contrastive
+
+
+import torch
+
+
+def find_label_with_th(output1, output2):
+    euclidean_distance = torch.nn.functional.pairwise_distance(output1, output2)
+    threshold = 0.5 * torch.max(euclidean_distance)  # Half of the maximum distance
+
+    label = torch.where(euclidean_distance > threshold, torch.tensor(1), torch.tensor(0))
+
+    return label
--- NNtorchSiamese.py
+++ NNtorchSiamese.py
@@ -6,15 +6,17 @@ from torch.optim import Adam
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
+import matplotlib.pyplot as plt
 
-from dataset import make_cryo_imgs_arr, makeDataset,makeSiameseDataset
+from dataset import make_cryo_imgs_arr, makeDataset, makeSiameseDataset
 import matplotlib.pyplot as plt
 # Get data
 # train = datasets.MNIST(root="data", download=True, train=True, transform=ToTensor())
 # dataset = DataLoader(train, 32)
 from livelossplot import PlotLosses
-from NN_torch_functions import euclidean_distance
+from NN_torch_functions import euclidean_distance, ContrastiveLoss,find_label_with_th
 import IPython
+
 # Create an instance of the PlotLosses class
 liveplot = PlotLosses()
 
@@ -27,6 +29,8 @@ test_data = makeSiameseDataset(images[:len_train], labels[:len_train])
 
 train_dataloader = DataLoader(training_data, batch_size=20, shuffle=True)
 test_dataloader = DataLoader(test_data, batch_size=20, shuffle=True)
+
+
 # examples = next(iter(train_dataloader))
 #
 # for label, img  in enumerate(examples):
@@ -51,8 +55,7 @@ class ImageClassifier(nn.Module):
             nn.Linear(64 * (360 - 6) * (360 - 6), 2)
         )
 
-
-    def forward(self, x1,x2):
+    def forward(self, x1, x2):
         # output1 = self.model(x[0])
         # # output2 = self.model(x[1])
         # output2 = (output1+3)*7
@@ -61,25 +64,24 @@ class ImageClassifier(nn.Module):
         return output1, output2
 
 
-
 # Instance of the neural network, loss, optimizer
 clf = ImageClassifier().to('cuda')
 opt = Adam(clf.parameters(), lr=1e-3)
 # Define your custom weights for each class
-class_weights = torch.tensor([1.0, 1.0]).to('cuda') # Higher weight for label 0
+class_weights = torch.tensor([1.0, 1.0]).to('cuda')  # Higher weight for label 0
 
 # Create the CrossEntropyLoss criterion with class weights
 loss_fn = nn.CrossEntropyLoss(weight=class_weights)
 
 loss_fn = nn.CrossEntropyLoss()
-
+contrastive_loss = ContrastiveLoss()
 # Training flow
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print("hi00")
     loss_values = []
     accuracy = []
-    num_epochs  =15
+    num_epochs = 15
     for epoch in range(num_epochs):
         logs = {}
         total_correct = 0
@@ -88,7 +90,7 @@ if __name__ == "__main__":
         total_val_loss = 0
 
         # model.train()
-        for i, (imagesA,imagesB, target) in enumerate(train_dataloader):
+        for i, (imagesA, imagesB, target) in enumerate(train_dataloader):
             # print("11")
             # print("22")
 
@@ -100,22 +102,20 @@ if __name__ == "__main__":
             labels = target.to('cuda:0')
 
             # Forward propagation
-            outputs = clf(imgsA,imgsB)
-
+            outputs = clf(imgsA, imgsB)
 
             # Calculating loss with softmax to obtain cross entropy loss
 
             # loss = criterion(outputs, labels)
-            outputs = euclidean_distance(outputs).to('cuda:0')
-            # labels = labels.squeeze()
+            # outputs = euclidean_distance(outputs).to('cuda:0')
+            labels = labels.squeeze()
 
-            labels_one_hot = nn.functional.one_hot(labels, num_classes=2)
+            # labels_one_hot = nn.functional.one_hot(labels, num_classes=2)
 
-
-            loss = loss_fn(outputs, labels_one_hot.float())  # ....>
+            # loss = loss_fn(outputs, labels_one_hot.float())  # ....>
 
             # loss = loss_fn(outputs, labels)  # ....>
-
+            loss = contrastive_loss(outputs[0], outputs[1], labels)
             opt.zero_grad()
             # Backward prop
             loss.backward()
@@ -127,9 +127,14 @@ if __name__ == "__main__":
             total_images += labels.size(0)
 
             # Obtaining predictions from max value
-            _, predicted = torch.max(outputs.detach(), 1)
+            # _, predicted = torch.max(outputs.detach(), 1)
 
+
+            predicted = find_label_with_th(outputs[0],outputs[1])
             # Calculate the number of correct answers
+
+
+
             correct = (predicted == labels).sum().item()
 
             total_correct += correct
@@ -150,35 +155,46 @@ if __name__ == "__main__":
             total_losss = 0
 
             for imagesA, imagesB, target in test_dataloader:
-
-                # imagesA, imagesB = data[0].to('cuda:0'), data[1].to('cuda:0')
-                # images = [imagesA, imagesB]
                 imgsA = imagesA.to('cuda:0')
                 imgsB = imagesB.to('cuda:0')
                 labels = target.to('cuda:0')
-                outputs = clf(imgsA,imgsB)
-
-                outputs = outputs[0] - outputs[1]
+                outputs = clf(imgsA, imgsB)
                 labels = labels.squeeze()
+                predicted = find_label_with_th(outputs[0], outputs[1])
 
-                _, predicted = torch.max(outputs.detach(), 1)
                 total += labels.size(0)
                 correct += (predicted == labels).sum().item()
                 total_losss += loss.item()
 
                 accuracy = correct / total
 
+                # Check if predicted label and correct label are equal
+                equal_labels = predicted == labels
+
+                # Iterate over the batch
+                # for i in range(len(equal_labels)):
+                #     if equal_labels[i].item() == 1:
+                #         # Retrieve the pair of images
+                #         imgA = imagesA[i][0]
+                #         imgB = imagesB[i][0]
+                #
+                #         outputs = clf(imgsA, imgsB)
+                #
+                #         euclidean_distance = torch.nn.functional.pairwise_distance(outputs[0], outputs[1])
+                #
+                #         plt.subplot(121)
+                #         plt.imshow(imgA.cpu().numpy())
+                #         plt.title("Image A")
+                #
+                #         plt.subplot(122)
+                #         plt.imshow(imgB.cpu().numpy())
+                #         plt.title("Image B")
+                #
+                #         plt.show()
+
             print('Test Accuracy of the model: {} %'.format(100 * correct / total))
-            logs['val_' + 'log loss'] = total_losss / total
-            logs['val_' + 'Accuracy'] = ((correct / total) * 100)
-        # logs['accuracy'] = accuracy
         liveplot.update(logs)
         # liveplot.draw()
 
     with open('/home/yoavharlap/PycharmProjects/yoav_network/model_siamese.pt', 'wb') as f:
         save(clf.state_dict(), f)
-
-
-
-
-
